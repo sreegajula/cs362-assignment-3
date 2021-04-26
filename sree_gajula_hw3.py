f757d3fff21c81a968f40483682883860be0f06e
Sree Gajula <gajulas@oregonstate.edu>
2021-04-25 19:57:37 -0700

    input validation

--- sree_gajula_hw3.py
+++ sree_gajula_hw3.py
@@ -1,10 +1,24 @@
 def main():
     check_leap()
 
-def check_leap ():
+
+def check_leap():
     year = input("Enter a year to see if that year is a leap year: ")
     leap = " is a leap year"
     not_leap = " is not a leap year"
+
+    while True:
+        try:
+            test = int(year)
+        except:
+            print('Enter numerical values only.')
+            return check_leap()
+            continue
+        if test < 1:
+            print('Enter a positive number.')
+            return check_leap()
+            continue
+        break
 
     if (int(year) % 4) == 0:
         if (int(year) % 100) == 0:
@@ -18,6 +32,7 @@ def check_leap ():
         print(year + not_leap)
 
     input("Press the Enter key to exit this program.")
+
 
 if __name__ == "__main__":
     main()
